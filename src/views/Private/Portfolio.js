e7a451fa5712386cb7075da598998cce27e70f8d
Jacky Yong <jacky@lesofi.asia>
2018-01-11 23:44:30 +0800

    Update Portfolio UI development

--- src/views/Private/Portfolio.js
+++ src/views/Private/Portfolio.js
@@ -2,17 +2,18 @@ import React from 'react';
 import EnhancedDataTable from '../../components/MaterialUi/DataTable/EnhancedDataTable';
 
 let counter = 0;
-function createData(name, calories, fat, carbs, protein) {
+function createData(symbol,availableBalance,pendingDeposit,reserved,total) {
   counter += 1;
-  return { id: counter, name, calories, fat, carbs, protein };
+  return { id: counter,action: 'Action', symbol, availableBalance, pendingDeposit, reserved, total };
 }
 
 const columnData = [
-  { id: 'name', numeric: false, disablePadding: false, label: 'Dessert (100g serving)' },
-  { id: 'calories', numeric: true, disablePadding: false, label: 'Calories' },
-  { id: 'fat', numeric: true, disablePadding: false, label: 'Fat (g)' },
-  { id: 'carbs', numeric: true, disablePadding: false, label: 'Carbs (g)' },
-  { id: 'protein', numeric: true, disablePadding: false, label: 'Protein (g)' },
+  { id: 'action', numeric: false, disablePadding: false, label: '+' },
+  { id: 'symbol', numeric: false, disablePadding: false, label: 'Symbol' },
+  { id: 'availableBalance', numeric: true, disablePadding: false, label: 'Available Balance' },
+  { id: 'pendingDeposit', numeric: true, disablePadding: false, label: 'Pending Deposit' },
+  { id: 'reserved', numeric: true, disablePadding: false, label: 'Reserved' },
+  { id: 'total', numeric: true, disablePadding: false, label: 'Total' },
 ];
 
 class Portfolio extends React.Component {
@@ -20,24 +21,24 @@ class Portfolio extends React.Component {
         super(props, context);
 
         this.state = {
-            order: 'asc',
-            orderBy: 'calories',
+            order: 'desc',
+            orderBy: 'availableBalance',
             selected: [],
             data: [
-              createData('Cupcake', 305, 3.7, 67, 4.3),
-              createData('Donut', 452, 25.0, 51, 4.9),
-              createData('Eclair', 262, 16.0, 24, 6.0),
-              createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-              createData('Gingerbread', 356, 16.0, 49, 3.9),
-              createData('Honeycomb', 408, 3.2, 87, 6.5),
-              createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-              createData('Jelly Bean', 375, 0.0, 94, 0.0),
-              createData('KitKat', 518, 26.0, 65, 7.0),
-              createData('Lollipop', 392, 0.2, 98, 0.0),
-              createData('Marshmallow', 318, 0, 81, 2.0),
-              createData('Nougat', 360, 19.0, 9, 37.0),
-              createData('Oreo', 437, 18.0, 63, 4.0),
-            ].sort((a, b) => (a.calories < b.calories ? -1 : 1)),
+              createData('BAT', 492119.39621730, 0.00000000, 0.00000000, 492119.39621730),
+              createData('BCC', 0.00001770, 0.00000000, 0.00000000, 0.00001770),
+              createData('BTG', 0.00000682, 0.00000000, 0.00000000, 0.00000682),
+              createData('BTC', 0.00000000, 0.00000000, 0.00000000, 0.00000000),
+              createData('LTC', 0.00000000, 0.00000000, 0.00000000, 0.00000000),
+              createData('DOGE', 0.00000000, 0.00000000, 0.00000000, 0.00000000),
+              createData('VTC', 0.00000000, 0.00000000, 0.00000000, 0.00000000),
+              createData('PPC', 0.00000000, 0.00000000, 0.00000000, 0.00000000),
+              createData('FTC', 0.00000000, 0.00000000, 0.00000000, 0.00000000),
+              createData('RDD', 0.00000000, 0.00000000, 0.00000000, 0.00000000),
+              createData('AAA', 0.00000000, 0.00000000, 0.00000000, 0.00000000),
+              createData('AAB', 0.00000000, 0.00000000, 0.00000000, 0.00000000),
+              createData('AAC', 0.00000000, 0.00000000, 0.00000000, 0.00000000),
+            ].sort((a, b) => (a.availableBalance < b.availableBalance ? -1 : 1)),
             page: 0,
             rowsPerPage: 5,
           };
