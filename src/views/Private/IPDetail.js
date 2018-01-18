9da5e9a7af041e287c68953c03227670712688ae
Jacky Yong <jacky@lesofi.asia>
2018-01-18 23:23:09 +0800

    Added Chart and IPDetail UI

--- src/views/Private/IPDetail.js
+++ src/views/Private/IPDetail.js
@@ -1,16 +1,38 @@
 import React from 'react';
+import Chart from './Chart';
+import { tsvParse, csvParse } from  'd3-dsv';
+import { timeParse } from 'd3-time-format';
+import { getData } from './ChartUtil';
+import { TypeChooser } from "react-stockcharts/lib/helper";
 
-const IPDetail=props=>{
-    return (
-        <div className='container'>
-            <div className='row'>
-                <br />
+
+class IPDetail extends React.Component {
+    componentDidMount() {
+		getData().then(data => {
+			this.setState({ data })
+		})
+    }
+    
+    render(){
+        if (this.state == null) {
+			return <div>Loading...</div>
+		}
+		
+        return (
+            <div className='container'>
+                <div className='row'>
+                    <br />
+                </div>    
+                <div className='row'>
+                    <h2>IP Detail</h2>
+                </div> 
+                <div className='row'>
+                   <Chart type={'svg'} data={this.state.data} width={500} />
+                </div>
             </div>    
-            <div className='row'>
-                <h2>IP Detail</h2>
-            </div> 
-        </div>    
-    )
+        )
+    }
+    
 }
 
 export default IPDetail;
