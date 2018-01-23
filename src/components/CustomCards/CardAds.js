1a04d01d9bbf2338953a7cd7f24f2d0fdf11d2a5
Jacky Yong <yong.jacky@gmail.com>
2018-01-23 16:52:45 +0800

    Improves Ads Banner and new ads images

--- src/components/CustomCards/CardAds.js
+++ src/components/CustomCards/CardAds.js
@@ -22,38 +22,18 @@ const styles = {
     }
 }
 
-const CardAds=({ipAvatar,ipCode,ipName})=>{
+const CardAds=({adsImage, adsText})=>{
     return (
-        <div style={styles.cardContainer}>
-            <div className="d-flex flex-row">
-                <div className="p-2">
-                    <div style={styles.icon}>
-                        <Avatar src={ipAvatar} />
-                    </div>
-                </div>
-                <div className="p-2">
-                    <div style={styles.profileContainer}> 
-                        <span style={styles.title}>{ipCode}</span>
-                        <br />
-                        <span>{ipName}</span>
-                    </div>
-                </div>
-            </div>
-            <div className="d-flex flex-row">
-                <div className="p-2">
-                    <div style={styles.descriptionContainer}> 
-                        <span>Description blah blah...</span>
-                    </div>
-                </div>
-            </div>        
-        </div> 
-    )
+        <div className='cardAdsContainer'>
+            <img src={adsImage} className='adsImage' />
+            <div className='adsText'>{adsText}</div>
+        </div>   
+     )
 }
 
 CardAds.propTypes = {
-    ipAvatar: PropTypes.string.isRequired,
-    ipCode: PropTypes.string.isRequired,
-    ipName: PropTypes.string.isRequired
+    adsImage: PropTypes.string.isRequired,
+    adsText: PropTypes.string.isRequired
 }
 
 export default CardAds;
--- src/views/Default/AdsCarousel.js
+++ src/views/Default/AdsCarousel.js
@@ -4,16 +4,63 @@ import range from 'lodash/range';
 import Card,{CardContent} from 'material-ui/Card';
 import CardAds from '../../components/CustomCards/CardAds';
 import demoImage from '../ImageAssets/ipAvatar/HH_ChanHouNam.jpg';
+import ads1 from '../ImageAssets/demoAds/ads1.jpg';
+import ads2 from '../ImageAssets/demoAds/ads2.jpg';
+import ads3 from '../ImageAssets/demoAds/ads3.jpg';
 
+const isMobile=(width)=> width <=500;
+
+const demoAds = [
+  { id: 1, code: 'ads1', desc: 'First advertisement description for demo....'},
+  { id: 2, code: 'ads2', desc: 'Second advertisement description for demo....'},
+  { id: 3, code: 'ads3', desc: 'Third advertisement description for demo....'},
+  { id: 4, code: 'ads1', desc: 'First advertisement description for demo....'},
+]
+
+const getDemoAdsImage=(code)=>{
+  switch (code) {
+    case 'ads1':
+      return ads1;
+    case 'ads2':  
+      return ads2;
+    case 'ads3':
+      return ads3;  
+    default:
+      break;
+  }
+}
 export default class AdsCarousel extends React.Component {
-    
+    constructor(){
+      super();
+      this.state = {
+        width: window.innerWidth
+      }
+    }
+
+    handleWindowSizeChange = () => {
+      this.setState({width: window.innerWidth });
+    }
+
+
+    createAds = () => demoAds.map((ads)=> {
+       return (
+         <div key={ads.id} style={{ height: '120px' }}>
+                <Card>
+                   <CardAds 
+                     adsImage={getDemoAdsImage(ads.code)}
+                     adsText={ads.desc}
+                    />  
+                 </Card> 
+          </div>
+       )
+    })
+
     createChildren = n => range(n).map(i => (
       <div key={i} style={{ height: '120px' }}>
                 <Card>
                    <CardAds 
-                     ipAvatar={demoImage}
-                     ipCode={'TDB'}
-                     ipName={'Teddy Boy'}
+                     adsImage={demoImage}
+                     adsText={'Teddy Boy'}
                     />  
                  </Card> 
       </div>
@@ -23,6 +70,7 @@ export default class AdsCarousel extends React.Component {
 
     
     componentWillMount() {
+        window.addEventListener('resize', this.handleWindowSizeChange)
         this.setState({
           children: [],
           activeItemIndex: 0,
@@ -30,31 +78,35 @@ export default class AdsCarousel extends React.Component {
     
         setTimeout(() => {
           this.setState({
-            children: this.createChildren(5),
+            children: this.createAds()
           })
         }, 100);
     }
     
-    
+    componentWillUnmount(){
+       window.removeEventListener('resize', this.handleWindowSizeChange );
+    }
+
     render(){
         const {
             activeItemIndex,
             children,
+            width
           } = this.state;
-
+          console.log('isMobile '+isMobile(width))
           return (
             <ItemsCarousel
               // Placeholder configurations
               enablePlaceholder
-              numberOfPlaceholderItems={5}
+              numberOfPlaceholderItems={3}
               minimumPlaceholderTime={1000}
               placeholderItem={<div style={{ height: 200 }}>Placeholder</div>}
       
               // Carousel configurations
-              numberOfCards={3}
-              gutter={12}
-              showSlither={true}
-              firstAndLastGutter={true}
+              numberOfCards={isMobile(width)?1:3}
+              gutter={3}
+              //showSlither={true}
+              firstAndLastGutter={false}
               freeScrolling={true}
       
               // Active item configurations
