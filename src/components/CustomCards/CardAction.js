bdc488936c4c5ec3b40618fcb0924c81eef01aea
Jacky Yong <jacky@lesofi.asia>
2018-01-23 11:58:19 +0800

    Minor update on Profile UI

--- src/components/CustomCards/CardAction.js
+++ src/components/CustomCards/CardAction.js
@@ -1,27 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const styles = {
-    cardContainer: {
-        height: '80px',
-        paddingLeft: '10px'
-    },
-    icon: {
-        width: '50px'
-    },
-    titleContainer: {
-        paddingTop: '5px',
-        width: '310px'
-    },
-    title: {
-        fontWeight: 'bold'
-    },
-    buttonContainer: {
-        paddingTop: '5px',
-        width: '100px'
-    }
-}
-
 const CardAction=({icon,title,subtitle,buttonText})=>{
     return (
         <div className='container cardActionContainer'>
@@ -48,28 +27,6 @@ const CardAction=({icon,title,subtitle,buttonText})=>{
             </div>
         </div>
        )
-        {/*
-        <div style={styles.cardContainer}>
-            <div className="d-flex flex-row">
-                <div className="p-2">
-                    <img src={icon} alt="" style={styles.icon} />
-                </div>
-                <div className="p-2">
-                    <div style={styles.titleContainer}> 
-                        <span style={styles.title}>{title}</span>
-                        <br />
-                        <span>{subtitle}</span>
-                    </div>
-                </div>
-                <div className="p-2">
-                    <div style={styles.buttonContainer}>  
-                    <button type="button" className="btn btn-primary">{buttonText}</button> 
-                    </div> 
-                </div>        
-            </div>    
-        </div> 
-        */}
-    
 }
 
 CardAction.propTypes = {
